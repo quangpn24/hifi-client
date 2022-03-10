0f405a1f7a100bb9fd3199d4cc7ba580bd57ff97
anhtt2211 <trananh22112001@gmail.com>
2022-03-10 12:30:32 +0700

    add colors constant

---
+++ src/constant/colors.ts
@@ -0,0 +1,17 @@
+export const PRIMARY_COLOR = '#514CDD';
+
+export const SECONDARY_COLOR = '#F8937E';
+
+export const TEXT_PRIMARY_COLOR = '#190134';
+
+export const TEXT_SECONDARY_COLOR = '#685879';
+
+export const TEXT_LINK_COLOR = '#5A69EB';
+
+export const SUCCESS_COLOR = '#68D8B8';
+
+export const ERROR_COLOR = '#FF713B';
+
+export const WARNING_COLOR = '#F7C96E';
+
+export const WHITE_COLOR = '#FFFFFF';
